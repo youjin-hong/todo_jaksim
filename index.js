c7982d2fd55842fe5f3d549ddc46bd72c96ff625
Jinny <asdfghjk1052@naver.com>
2024-08-23 14:30:17 +0900

    index.js에서 view경로 명시

--- index.js
+++ index.js
@@ -17,39 +17,38 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "./views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-	res.render("index");
+  res.render("index");
 });
 
 app.use("/", postRoutes);
 
 async function start() {
-	const client = await connectDB();
-	await Post.injectDB(client);
+  const client = await connectDB();
+  await Post.injectDB(client);
 
-	if (process.env.VERCEL) {
-		console.log("Vercel 환경에서는 서버를 시작하지 않습니다.");
-	} else {
-		app.listen(port, () => {
-			console.log("서버 실행중...");
-		});
-	}
+  if (process.env.VERCEL) {
+    console.log("Vercel 환경에서는 서버를 시작하지 않습니다.");
+  } else {
+    app.listen(port, () => {
+      console.log("서버 실행중...");
+    });
+  }
 
-
-	process.on("SIGINT", async () => {
-		try {
-			await client.close();
-			console.log("정상 DB 연결 종료");
-			process.exit(0);
-		} catch (err) {
-			console.error("오류에 의한 DB 연결 종료", err);
-			process.exit(1);
-		}
-	});
+  process.on("SIGINT", async () => {
+    try {
+      await client.close();
+      console.log("정상 DB 연결 종료");
+      process.exit(0);
+    } catch (err) {
+      console.error("오류에 의한 DB 연결 종료", err);
+      process.exit(1);
+    }
+  });
 }
 
 start();
