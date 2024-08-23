1ee1641d0a2632f6561f092be0b6fba45be1631f
Jinny <asdfghjk1052@naver.com>
2024-08-23 12:44:17 +0900

    index.js 수정

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
-app.set('views', path.join(__dirname, 'views'));  // 삭제해야할 수도 있는 코드
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
